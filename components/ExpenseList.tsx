e5633b60a9ed5102d8e6eab2fbb413e0da32598b
unknown <kazibadrul03@gmail.com>
2025-11-05 00:59:45 +0600

    fix typo

--- components/ExpenseList.tsx
+++ components/ExpenseList.tsx
@@ -130,7 +130,6 @@ const ExpenseList = () => {
                     : ""
                 }`}
               >
-                =
                 <div className="flex justify-between">
                   <div className="flex items-center gap-4">
                     <div
